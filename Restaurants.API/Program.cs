5f395d47b573a2a98e116b6b04fab70c6dd60542
Vitaliy Derda <derdavitaliy@gmail.com>
2025-03-22 14:13:57 +0200

    feat: added persistance to the infrastructure project, added seeds

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,12 +1,20 @@
+using Restaurants.Infrastructure.Extensions;
+using Restaurants.Infrastructure.Seeders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantsSeeder>();
+
+await seeder.SeedAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(restaurant => restaurant.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(restaurant => restaurant.Dishes)
+            .WithOne()
+            .HasForeignKey(dish => dish.RestaurantId);
+    }
+}
