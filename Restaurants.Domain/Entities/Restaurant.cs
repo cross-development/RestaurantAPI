966e40c03b038c6f26b34e136c57209a958b8e70
Vitaliy Derda <derdavitaliy@gmail.com>
2025-03-22 13:05:54 +0200

    feat: added clean architecture projects, added entities

---
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace Restaurants.Domain.Entities;
+
+public class Restaurant
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public bool HasDelivery { get; set; }
+
+    public string? ContactEmail { get; set; }
+    public string? ContactNumber { get; set; }
+
+    public Address? Address { get; set; }
+    public List<Dish> Dishes { get; set; } = new();
+}
